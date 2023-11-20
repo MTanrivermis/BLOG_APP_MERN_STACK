fefdf4964e4a482c4ad1fde6d6105fc7651d6414
Q4119-Midnight <tanrivermis.mehmet@gmail.com>
2023-11-20 10:02:59 +0300

    Setup NodeJS Server Sıde System

---
+++ Backend/index.js
@@ -0,0 +1,45 @@
+"use strict"
+
+/* ExpressJS Start */
+const express = require('express'); 
+const app = express() 
+/* ------------------------------------------------------------------------- */
+
+/* ENV */
+require('dotenv').config()
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 8000
+/* ------------------------------------------------------------------------- */
+
+app.all('/',(req,res)=> {
+    res.send({
+        error: false,
+        message: 'Welcome to Blog App server!!'
+    })
+})
+
+/* ------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------------------------- */
+// DEVELOPER SIDE PORT SYSTEM
+app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`))
---
+++ Backend/readme.md
@@ -0,0 +1,2 @@
+# npm init -y
+# $ npm i express dotenv express-async-errors mongoose cors
